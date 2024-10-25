b6690de8638a731ec9fc90a0fb362569808d1c5a
Tony Montana <kbpminiyarov@gmail.com>
2024-10-25 23:38:06 +0300

    5 dz

--- homework5/student.cpp
+++ homework5/student.cpp
@@ -5,65 +5,48 @@ using namespace std;
 
 class Student {
 private:
-    const string name, surname;
+    const string name = "Ryan";
+    const string surname = "Gosling";
     string group;
-    unsigned int age;
-    unsigned int HP, XP;
-    unsigned int money;
-    &studentTime (int hours, int min, int sec);
-    &Time (int H, int M, int S);
+    int age;
+    int HP, XP;
+    int money;
+    float studentTime;
+    float Time;
 public:
-    unsigned short int grade;
-    float wage;
+    short int grade;
+    float wage = 10;
 
-    Student(string p_name = "John", string p_surname = "Doe", int p_age = 19, string p_group = "", unsigned int p_hp = 100, unsigned int p_money = 100, int p_xp = 0, unsigned int p_grade = 10, float p_wage = 10):
-        name(p_name), surname(p_surname), age(p_age), group(p_group), HP(p_hp), money(p_money), XP(p_xp), grade(p_grade), wage(p_wage)
-    {
-        resetTime();
+    Student (string p_group, int p_age, int p_HP, int p_XP, int p_money, float p_studentTime, float p_Time) {
+        group = p_group;
+        age = p_age;
+        HP = p_HP;
+        XP = p_XP;
+        money = p_money;
+        studentTime = p_studentTime;
+        Time = p_Time;
     }
 
-// Работа со временем
+// Time managing
     void resetTime() {
-        studentTime.hours = 0;
-        studentTime.min = 0;
-        studentTime.sec = 0;
+        studentTime = 0;
     }
 
-    void addTime(unsigned int t_h, unsigned int t_m, unsigned int t_s) {
-        studentTime.hours += t_h;
-        studentTime.min += t_m;
-        studentTime.sec += t_s;
-        while (studentTime.sec >= 60) {
-            studentTime.min++;
-            studentTime.sec -= 60;
-        }
-        while (studentTime.min >= 60) {
-            studentTime.hours++;
-            studentTime.min -= 60;
-        }
+    void addTime(int t_h) {
+        studentTime += t_h;
     }
 
-    void addDayTime(unsigned int t_h, unsigned int t_m, unsigned int t_s) {
-        Time.H += t_h;
-        Time.M += t_m;
-        Time.S += t_s;
-        while (Time.S >= 60) {
-            Time.M++;
-            Time.S -= 60;
-        }
-        while (Time.M >= 60) {
-            Time.H++;
-            Time.M -= 60;
-        }
-        if (Time.H >= 24) {
-            Time.H = Time.H % 24;
+    void addDayTime(int t_h) {
+        Time += t_h;
+        while (Time >= 24) {
+            Time = Time - 24;
         }
     }
 
 
-// Действия
+// ACTIONS
     // Еда: 1-гречка, 2-шаверма, 3-поход в столовую
-    void Poel(unsigned short int act){
+    void Poel(int act){
 
         if (act == 1 && money < 50 || act == 2 && money < 200 || act == 3 && money < 300){
             cout << "Not enough money!";
@@ -74,22 +57,25 @@ public:
             case 1:
                 money -= 50;
                 HP += 10;
-                addTime(0,30,0);
-                addDayTime(0,30,0);
+                addTime(0.5);
+                addDayTime(0.5);
+                cout << "You ate grecha.";
                 break;
             
             case 2:
                 money -= 200;
                 HP += 30;
-                addTime(0,30,0);
-                addDayTime(0,30,0);
+                addTime(0.5);
+                addDayTime(0.5);
+                cout << "You ate shawarma.";
                 break;
 
             case 3:
                 money -= 300;
                 HP += 50;
-                addTime(0,30,0);
-                addDayTime(0,30,0);
+                addTime(0.5);
+                addDayTime(0.5);
+                cout << "You went to cafeteria.";
                 break;
             
             default:
@@ -97,109 +83,108 @@ public:
         }
     }
 
-    // Учимся
-    void Study(unsigned int t_h, unsigned int t_m, unsigned int t_s) {
-        unsigned short int damage = 20;
-        addTime(t_h, t_m, t_s);
-        addDayTime(t_h, t_m, t_s);
+    // STUDYING
+    void Study(int t_h) {
+        int damage = 20;
+        addTime(t_h);
+        addDayTime(t_h);
 
-        wage += 0.1 * (t_h + (t_m/60) + (t_s/3600)); // ЗП зависит от кол-ва знаний
+        wage += 0.1 * (t_h); // More knowledge - more wage
 
-        XP += 10 * (t_h + (t_m/60) + (t_s/3600));
+        XP += 10 * (t_h);
 
-        if (studentTime.hours >= 24) {
+        if (studentTime >= 24) {
             damage *= 3;
         }
 
-        if (Time.H <= 6) {
-            damage *= 1.5; // Наказываем ночной бот
+        if (Time <= 6) {
+            damage *= 2; // Restrict nighttime study
         }
 
         HP -= damage;
     }
 
-    // Работаем
-    void Work(unsigned int t_h, unsigned int t_m, unsigned int t_s) {
-        unsigned short int damage = 20;
-        addTime(t_h, t_m, t_s);
-        addDayTime(t_h, t_m, t_s);
+    // WORKING
+    void Work(int t_h) {
+        int damage = 20;
+        addTime(t_h);
+        addDayTime(t_h);
 
-        money += wage * (t_h + (t_m/60) + (t_s/3600));
+        money += wage * (t_h);
 
-        if (studentTime.hours >= 24) {
+        if (studentTime >= 24) {
             damage *= 3;
         }
 
-        if (Time.H <= 6) {
-            damage *= 1.5; // Наказываем ночные смены
+        if (Time <= 6) {
+            damage *= 2; // Restrict nighttime work
         }
 
         HP -= damage;
     }
 
-    // Спим
-    void Sleep(unsigned int t_h, unsigned int t_m, unsigned int t_s) {
-        unsigned short int heal = 10;
+    // SLEEP
+    void Sleep(int t_h) {
+        int heal = 10;
 
-        if ((t_h + (t_m/60) + (t_s/3600)) >= 6) {
+        HP += heal * (t_h);
+        addTime(t_h);
+        addDayTime(t_h);
+        if ((t_h) >= 6) {
             resetTime();
         }
-
-        HP += heal * (t_h + (t_m/60) + (t_s/3600));
-        addTime(t_h, t_m, t_s);
-        addDayTime(t_h, t_m, t_s);
     }
     
-    // Развлекаемся: 1-алкоголизм, 2-муз. репетиция, 3-свидание, 4-скролл ленты
-    void Entertain(unsigned short int act, unsigned int t_h, unsigned int t_m, unsigned int t_s) {
-        unsigned short int heal;
+    // Entertainment: 1 - acloholism, 2 - music session, 3 - dating, 4 - scrolling
+    void Entertain(int act, int t_h) {
+        int heal;
 
         if (act == 1 && money < 200 || act == 2 && money < 100 || act == 3 && money < 300){
             cout << "Poor!";
             return;
         }
 
-        addTime(t_h, t_m, t_s);
-        addDayTime(t_h, t_m, t_s);
+        addTime(t_h);
+        addDayTime(t_h);
 
         switch (act) {
             case 1:
                 heal = 10;
-                if (studentTime.hours >= 24) {
+                if (studentTime >= 24) {
                     heal /= 3;
                 }
 
                 money -= 200;
-                HP += heal * (t_h + (t_m/60) + (t_s/3600));
+                HP += heal * (t_h);
                 break;
             
             case 2:
                 heal = 20;
-                if (studentTime.hours >= 24) {
+                if (studentTime >= 24) {
                     heal /= 3;
                 }
 
                 money -= 100;
-                HP += 30
+                HP += heal * t_h;
                 break;
 
             case 3:
                 heal = 30;
-                if (studentTime.hours >= 24) {
+                if (studentTime >= 24) {
                     heal /= 3;
                 }
 
                 money -= 300;
-                HP += heal * (t_h + (t_m/60) + (t_s/3600));
+                HP += heal * (t_h);
                 break;
             
             case 4:
                 heal = 10;
-                if (studentTime.hours >= 24) {
+                if (studentTime >= 24) {
                     heal /= 3;
                 }
 
-                HP += heal * (t_h + (t_m/60) + (t_s/3600));
+                HP += heal * (t_h);
                 break;            
 
             default:
@@ -208,7 +193,7 @@ public:
     }
 
 
-// Информация
+// INFO
     int getHP() {
         return HP;
     }
@@ -217,8 +202,20 @@ public:
         return money;
     }
 
+    float getStudentTime() {
+        return studentTime;
+    }
+
+    float getDayTime() {
+        return Time;
+    }
+
     void getTimeLeft() {
-        cout << 24 - (Time.H) << " hours, " << 60 - Time.M << " minutes, " << 60 - Time.S << " seconds left.";
+        cout << 24 - (Time) << " hours left in day.";
+    }
+
+    int getXP() {
+        return XP;
     }
 
     void printInfo() {
@@ -228,9 +225,9 @@ public:
         cout << "Health level: " << HP << endl;
         cout << "Money left: " << money << endl;
         cout << "Gained Experience: " << XP << endl;
-        cout << "Semester grade: " << grade << endl;
-        cout << "Current time: " << Time.H << ":" << Time.M << ":" << Time.S << endl;
-        cout << "Time since last GOOD sleep: " << studentTime.hours << ":" << studentTime.min << ":" << studentTime.sec << endl;
+        cout << "Semester grade: " << 10 << endl;
+        cout << "Current time: " << Time << endl;
+        cout << "Time since last GOOD sleep: " << studentTime << endl;
     }
 
 
@@ -248,8 +245,67 @@ public:
 
 
 int main() {
-    Student bublik("Ryan", "Gosling", 19, "B04-304", 100, 100, 0, 10);
-    bublik.printInfo();
+    Student bublik("B04-304", 19, 100, 0, 100, 0, 0);
+    bool flag = 0;
+    float Time = bublik.getDayTime();
+    float XP = bublik.getXP();
+    float prev_time = 0;
 
+    while (not flag){
+        if (prev_time > Time) {
+            cout << "Another miserable day passed...\n";
+            bublik.printInfo();
+        }
 
+        prev_time = Time;
+        cout << "Choose your action:\n1. Eat \n2. Work \n3. Study \n4. Sleep \n5. Entertainment \n6. Get info \n";
+        int act;
+        int t_h;
+        cin >> act;
+        switch (act) {
+            case 1:
+                cout << "Pick your food: \n1.Grecha \n2.Shawarma \n3.Go to cafeteria \n";
+                int eda;
+                cin >> eda;
+                bublik.Poel(eda);
+                break;
+            case 2:
+                cout << "How many hours will you work for? \n";
+                cin >> t_h;
+                bublik.Work(t_h);
+                break;
+            case 3:
+                cout << "How many hours will you study for? \n";
+                cin >> t_h;
+                bublik.Study(t_h);
+                break;
+            case 4:
+                cout << "How much time will you sleep for? \n";
+                cin >> t_h;
+                bublik.Sleep(t_h);
+                break;
+            case 5:
+                cout << "Choose your entertainment (number): \n1. Alcoholism \n2. Play music \n3. Go on a date \n4. Scroll through your phone \n";
+                int ent;
+                cin >> ent;
+                cout << "Number of hours: \n";
+                cin >> t_h;
+                bublik.Entertain(ent, t_h);
+                break;
+            case 6:
+                bublik.printInfo();
+                break;
+        }
+
+        bool flag = bublik.isDead();
+        if (flag) {
+            break;
+        }
+    }
+
+    if (bublik.isDead()) {
+        bublik.sdox();
+    }
+
+    return 0;
 }
