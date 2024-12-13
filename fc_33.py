48cebca754ee607d4efd96d065a50a19b74cfb55
Tony Montana <kbpminiyarov@gmail.com>
2024-12-13 18:38:25 +0300

    removal

--- fc_33.py
+++
@@ -1,6 +0,0 @@
-with open('FC_3.txt', 'r') as input_file:
-    with open('column1.txt', 'w') as column1_file, open('column2.txt', 'w') as column2_file:
-        for line in input_file:
-            col1, col2 = line.strip().split(',')
-            column1_file.write(col1 + '\n')
-            column2_file.write(col2 + '\n')
